de31f8b8a8ed546e48485b4c3a9f0e571876c686
manjunath343 <manjunathmajestic343@gmail.com>
2025-03-21 12:13:29 +0530

    added toast/verification for forgot-password

--- src/app/api/users/changePassword/route.ts
+++ src/app/api/users/changePassword/route.ts
@@ -14,7 +14,7 @@ export async function POST(request:NextRequest) {
         if(!user) {
             return NextResponse.json({error:"User does not exist",success:false},{status:400});
         }
-        const  hashedPassword= await bcrypt.hash(password, 10);
+        const  hashedPassword= await bcryptjs.hash(password, 10);
         user.password = hashedPassword;
         await user.save();
         const mailOptions = {
