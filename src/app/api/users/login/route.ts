bf765880a28cf0019a6b7ddcb0dcf89236435a3a
Adithya <143900126+Adithya404@users.noreply.github.com>
2025-04-04 12:02:43 +0530

    "Modify cookie expiry"

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,46 +1,63 @@
-import {connect} from "@/dbConfig/dbConfig";
+import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
-import {NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 connect();
- 
-export async function POST(request:NextRequest) {
-    try {
-        const reqBody = await request.json();
-        const {email,password}=reqBody;
-        const user = await User.findOne({email})
-        if(!user){
-            return NextResponse.json({message:"User does not exist",success:false},{status:400})
-        }
-        const validPassword = await bcryptjs.compare(password,user.password)
-        if(!validPassword){
-            return NextResponse.json({message:"Invalid Password",success:false},{status:400})
-        }
-        const tokenData = {
-            id:user._id,
-            username:user.username,
-            email:user.email
-        }
-        const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET!,{expiresIn:"1d"})
-        const response = NextResponse.json(
-            {
-                message:"Login successful",
-                success:true
-            },
-            {
-                status:200
-            }
-        )
-        response.cookies.set("token",token,{httpOnly:true,
-            path:"/"})
-        response.cookies.set("userType", "user", { httpOnly: true, path: "/" });
-        return response
 
-        console.log(reqBody);
-    } catch (error:any) {
-        return NextResponse.json({error:error.message+"hello catch"},{status:500})
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { message: "User does not exist", success: false },
+        { status: 400 }
+      );
     }
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { message: "Invalid Password", success: false },
+        { status: 400 }
+      );
+    }
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json(
+      {
+        message: "Login successful",
+        success: true,
+      },
+      {
+        status: 200,
+      }
+    );
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+    response.cookies.set("userType", "user", {
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+
+    return response;
+
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
 }
- 
--- src/app/api/vendors/login/route.ts
+++ src/app/api/vendors/login/route.ts
@@ -1,49 +1,65 @@
-import {connect} from "@/dbConfig/dbConfig";
+import { connect } from "@/dbConfig/dbConfig";
 import Vendor from "@/models/vendorModel";
-import {NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 connect();
- 
-export async function POST(request:NextRequest) {
-    try {
-        const reqBody = await request.json();
-        const {email,password}=reqBody;
-        const vendor = await Vendor.findOne({email})
-        if(!vendor){
-            return NextResponse.json({error:"Vendor does not exist",success:false},{status:400})
-        }
-        const validPassword = await bcryptjs.compare(password,vendor.password)
-        if(!validPassword){
-            return NextResponse.json({error:"Invalid Password",success:false},{status:400})
-        }
-        if(vendor.isVerified === false) {
-            return NextResponse.json({ error: "Vendor is not verified",status: false }, { status: 401 });
-        }
-        const tokenData = {
-            id:vendor._id,
-            username:vendor.username,
-            email:vendor.email
-        }
-        const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET!,{expiresIn:"1d"})
-        const response = NextResponse.json(
-            {
-                message:"Login successful",
-                success:true
-            },
-            {
-                status:200
-            }
-        )
-        response.cookies.set("token",token,{httpOnly:true,
-            path:"/"})
-        response.cookies.set("userType", "vendor", { httpOnly: true, path: "/" });
-        return response
 
-        console.log(reqBody);
-    } catch (error:any) {
-        return NextResponse.json({error:error.message},{status:500})
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    const vendor = await Vendor.findOne({ email });
+    if (!vendor) {
+      return NextResponse.json(
+        { error: "Vendor does not exist", success: false },
+        { status: 400 }
+      );
     }
+    const validPassword = await bcryptjs.compare(password, vendor.password);
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Invalid Password", success: false },
+        { status: 400 }
+      );
+    }
+    if (vendor.isVerified === false) {
+      return NextResponse.json(
+        { error: "Vendor is not verified", status: false },
+        { status: 401 }
+      );
+    }
+    const tokenData = {
+      id: vendor._id,
+      username: vendor.username,
+      email: vendor.email,
+    };
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+    const response = NextResponse.json(
+      {
+        message: "Login successful",
+        success: true,
+      },
+      {
+        status: 200,
+      }
+    );
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+    response.cookies.set("userType", "vendor", {
+      httpOnly: true,
+      path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+    return response;
+
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
- 
